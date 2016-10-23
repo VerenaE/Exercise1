8b968cce1946d558119802c147f986d26d9df2f6
VerenaE <verena.priesack@tum.de>
2016-10-23 15:34:44 +0200

    Add files via upload

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * 
  */
@@ -6,13 +8,27 @@
  * @author Mohsen
  *
  */
-public class HelloWorld {
+class HelloUser{
 
+	Scanner sc = new Scanner(System.in);
+    String userName = sc.next();
+
+	public void greetUser() {
+		System.out.println("Hello " +userName +"!");
+	}	
+}
+
+
+public class HelloWorld {
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		System.out.println("Hello ASE2016 - how are you? :)");
+		System.out.println("Please enter your name: ");
+		
+		HelloUser hu = new HelloUser();
+		hu.greetUser();
 	}
-
 }
+
